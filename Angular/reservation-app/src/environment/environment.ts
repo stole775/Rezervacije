92b472190df2e29f4f587fb540eb41d42b4ceedf
stole775 <mladen.stole@gmail.com>
2024-11-06 22:26:23 +0100

    v1.0

--- Angular/reservation-app/src/environment/environment.ts
+++ Angular/reservation-app/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://188.245.154.242:8080/api'
+    apiUrl: 'https://188.245.154.242/api'
     //apiUrl: 'http://89.23.68.241:8080'
   };
   
