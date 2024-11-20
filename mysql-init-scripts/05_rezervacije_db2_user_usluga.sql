e4431d0bf61ab0b19aa8c50d10f667050d06ebfd
Mladen Stolic <mladen.stolic@v21.fazi.rs>
2024-11-20 11:43:08 +0100

    v1.1

--- mysql-init-scripts/05_rezervacije_db2_user_usluga.sql
+++ mysql-init-scripts/05_rezervacije_db2_user_usluga.sql
@@ -31,7 +31,5 @@ INSERT INTO `user_usluga` (`user_id`, `usluga_id`, `cena`, `trajanje`) VALUES
 (6, 1, 26, 30),      -- Korisnik 6, Usluga 1, Cena 26, Trajanje 30 min
 (1, 2, 100, 20),     -- Korisnik 1, Usluga 2, Cena 100, Trajanje 20 min
 (6, 2, 1008, 25),    -- Korisnik 6, Usluga 2, Cena 1008, Trajanje 25 min
-(5, 3, 10085, 30),   -- Korisnik 5, Usluga 3, Cena 10085, Trajanje 30 min
-(5, 10, 1500, 40),   -- Korisnik 5, Usluga 10, Cena 1500, Trajanje 40 min
-(5, 11, 2000, 50);   -- Korisnik 5, Usluga 11, Cena 2000, Trajanje 50 min
+(5, 3, 10085, 30);   -- Korisnik 5, Usluga 11, Cena 2000, Trajanje 50 min
 UNLOCK TABLES;
