fbdeca3f6a3aaa7a23c5033fa96cdc8068074657
Mladen Stolic <mladen.stolic@v21.fazi.rs>
2025-03-05 14:36:51 +0100

    Izmena docker compose

--- Angular/reservation-app/src/environment/environment.ts
+++ Angular/reservation-app/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-     apiUrl: 'https://rezervisime.com/api'
+    apiUrl: 'https://rezervisime.com/api'
      //apiUrl: 'http://localhost:8080/api'  
   };
   
